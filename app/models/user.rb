d8204f138571a1c4dcc49619a4915ea3659d70c4
Hendra Gunawan <hengun.bless@gmail.com>
2013-01-08 00:13:06 +0700

    fix some bug so app ready to clone

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User
   include Mongoid::Timestamps
   include Codecamp::Ext::UserAuthentication
 
-  field :firstname,                 :type => String, :limit => 100, :default => '', :null => true
-  field :lastname,                  :type => String, :limit => 100, :default => '', :null => true
-  field :bio,                       :type => String, :limit => 200
-  field :website,                   :type => String, :limit => 200
-  field :location,                  :type => String, :limit => 200
+  field :firstname,                 :type => String
+  field :lastname,                  :type => String
+  field :bio,                       :type => String
+  field :website,                   :type => String
+  field :location,                  :type => String
   field :birthday,                  :type => Time
 
   field :identity_url,              :type => String
@@ -26,15 +26,11 @@ class User
   embeds_one :contact_information, cascade_callbacks: true
   embeds_one :social_media, cascade_callbacks: true
 
-
-  validates_inclusion_of :language, :in => AVAILABLE_LOCALES, :if => lambda { AppConfig.enable_i18n }
-  validates_inclusion_of :role,  :in => ROLES
-
   validates_length_of       :firstname,     :maximum => 100
   validates_length_of       :lastname,     :maximum => 100
 
   # accepts nested attributes
-  accepts_nested_attributes_for :profile, :business_information
+  accepts_nested_attributes_for :profile
 
   # return fullname if present, otherwise login name
   def display_name
--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'# require 'rails/all'
+# require 'rails/all'
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
+# require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 require "omniauth"
 
 if defined?(Bundler)
@@ -57,7 +58,7 @@ module RailsBaseApplication
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
